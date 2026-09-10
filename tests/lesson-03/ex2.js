d9200e6aa9f148cc27c4976a51e05ecf0dd1f5ca
Anh Nguyen <nguyenanh1904.vn@gmail.com>
2026-09-10 23:07:08 +0700

    feat: complete js exercises

---
+++ tests/lesson-03/ex2.js
@@ -0,0 +1,7 @@
+const height = 170;
+const height1 = height % 100;
+const idealWeight = (height1*9)/10;
+const maxWeight = height1;
+const minWeight = (height1*8)/10;
+
+console.log(`Cân nặng lý tưởng: ${idealWeight}kg, Cân nặng tối đa: ${maxWeight}kg, Cân nặng tối thiểu: ${minWeight}kg`);
